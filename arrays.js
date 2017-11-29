4682ec2a37c0497e7b7ae96f7ef93a63398ed20c
Artthik Sellathurai <artthik.sellathurai@students.unibe.ch>
2017-11-29 18:05:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,7 +32,7 @@ function accessElementInArray(array, index){
 }
 
 function removeElementFromEndOfArray(array){
-  return 0;
+  return array.pop();
 }
 
 function removeElementFromBeginningOfArray(array){
