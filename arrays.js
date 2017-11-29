7b793026a3c73e6289dff53c6c5204193220dfb4
Artthik Sellathurai <artthik.sellathurai@students.unibe.ch>
2017-11-29 17:29:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,6 +4,10 @@ function addElementToBeginningOfArray(array, element){
   return [element, ...array];
 }
 
+function addElementToEndOfArray(array, element){
+  return [...array, element];
+}
+
 function destructivelyAddElementToBeginningOfArray(array, element){
   
 }
